8341ddd6912223bbe1a0f427184a9d8cd3dc62c7
Toby Thain <toby@telegraphics.net>
2024-06-23 11:51:33 -0400

    Working serial.

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
  ┃ 2   4   6 ┃  ISP header socket
  ┃ 1   3   5 ┃  on Digispark carrier protoboard
  ┗━━━     ━━━┛
+ See: https://www.pololu.com/docs/0J36/all
  1:       = MISO        = Target pin 1
  2: Red   = Target VDD 5V
  3: Green = SCK         = pin 2
@@ -24,12 +25,12 @@
 // I/O Map
 // PB5: _RESET; avoid for i/o
 // PB4:
-// PB3:
-// PB2: SCK: serial clock input
-// PB1: MISO: SPI Host Data Input / Client Data Output
-// PB0: MOSI: SPI Host Data Output / Client Data Input
+// PB3: ISP RX (to computer)
+// PB2: sensor SCK
+// PB1:
+// PB0: sensor SDA
 
-enum __attribute__ ((__packed__)) {
+enum {
    SCD41_ADDRESS = 0x62
 };
 
@@ -38,16 +39,71 @@ static char i2c_get_serial_number[] = {
    0x36, 0x82
 };
 
+#define RXMASK 0b1000
+
+// N.B. clock is 16.5MHz
+uint8_t d = 211; // discovered by testing - see serial_test()
+
+void delay(uint8_t k) {
+   while(k--) { // target 8 cycles per iteration
+      __builtin_avr_nops(3); // + about 5 cycles of loop overhead
+   }
+}
+
+#define SERIAL_LO() if(1){ PORTB &= ~RXMASK; }
+#define SERIAL_HI() if(1){ PORTB |= RXMASK; }
+
+void send(uint8_t b) {
+   SERIAL_LO(); // Start bit
+   delay(d); // 9600bps: 104.17µs per bit
+
+   for(uint8_t i = 0; i < 8; ++i) {
+      if(b & 1) {
+         SERIAL_HI();
+      } else {
+         SERIAL_LO();
+      }
+      b >>= 1;
+      delay(d);
+   }
+
+   SERIAL_HI(); // Stop bit
+   delay(d);
+}
+
+void serial_test() {
+   // try different values of delay to find the range that works
+   // on my Digispark Tiny, the working range is 201..222 inclusive.
+
+   for(d=1;d;){
+      PORTB ^= 0b10; // toggle LED
+      for(uint8_t i = 0; i < 2; ++i) {
+         send('.'); send('.'); send('.'); send('(');
+         send('0'+(d/100));
+         send('0'+((d/10)%10));
+         send('0'+(d%10));
+         send(')');
+         send('\r'); send('\n');
+         _delay_us(500);
+      }
+      ++d;
+      _delay_ms(500);
+   }
+}
+
 int main() {
+   DDRB = 0b1010; // set serial RX and LED pin to output
+   SERIAL_HI(); // set line idle
+   _delay_ms(200);
+
    char buf[9] = { (SCD41_ADDRESS << 1) | 0 /*read*/ };
 
-   USI_I2C_Master_Start_Transmission(i2c_get_serial_number, sizeof(i2c_get_serial_number));
+   //USI_I2C_Master_Start_Transmission(i2c_get_serial_number, sizeof(i2c_get_serial_number));
 
-   USI_I2C_Master_Start_Transmission(buf, sizeof(buf));
+   //USI_I2C_Master_Start_Transmission(buf, sizeof(buf));
 
+   send('O'); send('K'); send('.'); send('\r'); send('\n');
 
-   for(;;);
-
-   #ifndef ARDUINO_avrdd
+#ifndef ARDUINO_avrdd
 #endif
 }
